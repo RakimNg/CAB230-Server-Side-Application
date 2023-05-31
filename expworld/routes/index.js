e4db830b68b470e769691458b7ad6cbd633e8fc1
RakimNg <105621141+RakimNg@users.noreply.github.com>
2023-06-01 00:33:17 +1000

    search movie route, update swagger json
    more filters open

--- expworld/routes/index.js
+++ expworld/routes/index.js
@@ -3,23 +3,11 @@ var router = express.Router();
 const authorization = require("../middleware/authorization.js");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
 
-router.get("/api/city", function (req, res, next) {
-  req.db
-    .from("city")
-    .select("name", "district")
-    .then((rows) => {
-      res.json({ Error: false, Message: "Success", City: rows });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ Error: true, Message: "Error in MySQL query" });
-    });
-});
 
 router.post("/api/city/update", authorization, function (req, res, next) {
   // const jwt = authorization.jwt
@@ -64,19 +52,7 @@ router.get("/api/city/:CountryCode", function (req, res, next) {
     });
 });
 
-router.get("/api/movie/:CountryCode", function (req, res, next) {
-  req.db
-    .from("basics")
-    .select()
-    .where("country", "=", req.params.CountryCode)
-    .then((rows) => {
-      res.json({ Error: false, Message: "Success", Movie: rows });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({ Error: true, Message: "Error in MySQL query" });
-    });
-});
+
 router.get("/movies/data/:imdbID", function (req, res, next) {
   req.db
     .from("basics")
@@ -102,13 +78,14 @@ router.get("/people/", function (req, res, next) {
       res.json({ Error: true, Message: "Error in MySQL query" });
     });
 });
+// authorization,
 router.get("/people/:ID", authorization, function (req, res, next) {
   req.db
     .from("principals")
     .select()
     .where("nconst", "=", req.params.ID)
     .then((rows) => {
-      res.json({ Error: false, Message: "Success", Movie: rows });
+      res.json({ Error: false, Message: "Success", Movies: rows });
     })
     .catch((err) => {
       console.log(err);
@@ -116,34 +93,43 @@ router.get("/people/:ID", authorization, function (req, res, next) {
     });
 });
 router.get("/movies/search", function (req, res, next) {
-  if (!req.body.primaryTitle) {
-    req.db
-      .from("basics")
-      .select()
-      .then((rows) => {
-        res.json({ Error: false, Message: "Success", Movie: rows });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({ Error: true, Message: "Error in MySQL query" });
-      });
+
+  let name = "";
+  let year = "";
+  let page = 1; // Current page number, default: 1
+
+  let limit = 100 // Number of records per page, default: 10
+
+  if (req.query.primaryTitle) {
+    name = req.query.primaryTitle;
 
   }
-  else {
-    const name = req.body.primaryTitle;
-    console.log(name)
-    req.db
-      .from("basics")
-      .select()
-      .where("primaryTitle", "LIKE", `%${name}%`)
-      .then((rows) => {
-        res.json({ Error: false, Message: "Success", Movie: rows });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({ Error: true, Message: "Error in MySQL query" });
-      });
+  if (req.query.page) {
+    page = parseInt(req.query.page)
   }
-
+  let offset = (page - 1) * limit; // Calculate the offset for pagination
+  console.log(page)
+  if (req.query.year) {
+    year = req.query.year;
+    console.log(year);
+  }
+  console.log(name);
+  req.db
+    .from("basics")
+    .select()
+    .where("primaryTitle", "LIKE", `%${name}%`)
+    .andWhere("year", "LIKE", `%${year}%`)
+    .limit(limit)
+    .offset(offset)
+    .then((rows) => {
+      res.json({ Error: false, Message: "Success", Movie: rows });
+      console.log("..."); // Move the console print here
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ Error: true, Message: "Error in MySQL query" });
+      return;
+    });
 });
 module.exports = router;
