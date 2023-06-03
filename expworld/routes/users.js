80c7ef698451b683187f0fce9bea8a0318932af5
RakimNg <105621141+RakimNg@users.noreply.github.com>
2023-06-03 15:29:59 +1000

    270 tests passed

--- expworld/routes/users.js
+++ expworld/routes/users.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const authorization = require("../middleware/authorization.js");
 const logout = require("../middleware/logout.js")
 const jwt = require('jsonwebtoken');
+const refresh = require('../middleware/refresh.js');
 const JWT_SECRET = process.env.JWT_SECRET;
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -13,38 +14,65 @@ router.get('/', function (req, res, next) {
 // router.get("/:email/profile", authorization, function (req, res, next) {
 
 // });
-router.get("/:email/profile", authorization, function (req, res, next) {
-  const email = req.params.email;
-  console.log("email is: " + email);
+router.route("/:email/profile")
+  .all(authorization)
+  .get(function (req, res, next) {
+    const email = req.params.email;
+    console.log("email is: " + email);
 
-  // Retrieve the data from the database
-  req.db.from("users")
-    .select("email", "firstName", "lastName")
-    .where("email", "=", email)
-    .first()
-    .then((result) => {
-      if (result) {
-        const data = {
-          email: result.email,
-          firstName: result.firstName,
-          lastName: result.lastName,
-        };
-        res.json(data); // Return the data as JSON response
-      } else {
-        res.status(404).json({ error: "User not found" });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).json({ error: "Internal server error" });
-    });
-});
-router.post("/refresh", authorization, function (req, res, next) {
-  // const jwt = authorization.jwt
+    // Retrieve the data from the database
+    req.db.from("users")
+      .select("email", "firstName", "lastName")
+      .where("email", "=", email)
+      .first()
+      .then((result) => {
+        if (result) {
+          const data = {
+            email: result.email,
+            firstName: result.firstName,
+            lastName: result.lastName,
+          };
+          res.json(data); // Return the data as JSON response
+        } else {
+          res.status(404).json({ error: "User not found" });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({ error: "Internal server error" });
+      });
+  })
+  .put(function (req, res, next) {
 
 
-});
-router.post("/logout", logout, function (req, res, next) {
+    const filter = {
+      "email": req.params.email,
+    };
+    if (!req.params.firstName || req.params.lastName || req.params.address || req.params.dob) {
+      res.status(400).json({ "error": true, message: `Request body incomplete: firstName, lastName, dob and address are required.` });
+    }
+    else {
+      const pop = {
+        "firstName": req.params.firstName,
+        "lastName": req.params.lastName,
+        "address": req.params.address,
+        "dob": req.params.dob
+
+      };
+      req.db('users').where(filter).update(pop)
+        .then(_ => {
+          res.status(200).json({ message: `Successful update ${req.params.email}` });
+        }).catch(error => {
+          res.status(500).json({ message: 'Database error - not updated' });
+        });
+    }
+
+
+
+
+  });
+
+router.post("/logout", authorization, function (req, res, next) {
   // const jwt = authorization.jwt
 
 
@@ -92,6 +120,34 @@ router.post("/register", function (req, res, next) {
     });
 });
 
+router.post("/refresh", authorization, function (req, res, res) {
+
+  const expires_in_1 = 600;
+  const expires_in_2 = 86400;
+  const exp = Math.floor(Date.now() / 1000) + expires_in_1;
+  const exp_ = Math.floor(Date.now() / 1000) + expires_in_2;
+  const token = jwt.sign({ exp, email }, JWT_SECRET);
+  const token_1 = jwt.sign({ exp_, email }, JWT_SECRET);
+
+  if (!expires_in_1 || !token) {
+    return res.status(400).json({ message: `Error updating token` });
+  } else {
+    return res.status(200).json({
+      bearerToken: {
+        token,
+        token_type: "Bearer",
+        expires_in: expires_in_1
+      },
+      refreshToken:
+      {
+        token_1,
+        token_type: "Refresh",
+        expires_in: expires_in_2
+      }
+    });
+  }
+
+})
 
 router.post("/login", function (req, res, next) {
   const email = req.body.email;
